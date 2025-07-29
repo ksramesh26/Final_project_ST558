d8403802143ac3a901bc61f0e61c6013f7da5b26
ksramesh26 <keshavramesh26@gmail.com>
2025-07-29 00:43:35 -0400

    Made api document and built final model

--- Diabetes_Health_Indicators_ST558/plumber.R
+++ Diabetes_Health_Indicators_ST558/plumber.R
@@ -1,44 +1,39 @@
-#
-# This is a Plumber API. You can run the API by clicking
-# the 'Run API' button above.
-#
-# Find out more about building APIs with Plumber here:
-#
-#    https://www.rplumber.io/
-#
-
+library(tidyverse)
+library(tidymodels)
 library(plumber)
+library(ranger)
 
-#* @apiTitle Plumber Example API
-#* @apiDescription Plumber example description.
+# Load and format data
+data_raw <- read_csv("../diabetes_binary_health_indicators_BRFSS2015.csv")
 
-#* Echo back the input
-#* @param msg The message to echo
-#* @get /echo
-function(msg = "") {
-    list(msg = paste0("The message is: '", msg, "'"))
-}
+diabetes_data <- data_raw %>%
+  mutate(
+    Diabetes_binary = factor(Diabetes_binary, levels = c(0, 1), labels = c("No Diabetes", "Diabetes")),
+    PhysActivity = factor(PhysActivity, levels = c(0, 1), labels = c("No", "Yes")),
+    HighChol = factor(HighChol, levels = c(0, 1), labels = c("No", "Yes")),
+    HighBP = factor(HighBP, levels = c(0, 1), labels = c("No", "Yes")),
+    AgeGroup = factor(Age, levels = 1:13, labels = c(
+      "18–24", "25–29", "30–34", "35–39", "40–44",
+      "45–49", "50–54", "55–59", "60–64", "65–69",
+      "70–74", "75–79", "80+"))
+  )
 
-#* Plot a histogram
-#* @serializer png
-#* @get /plot
-function() {
-    rand <- rnorm(100)
-    hist(rand)
-}
+# Final model and recipe
+diabetes_rec3 <- recipe(Diabetes_binary ~ BMI + AgeGroup + PhysActivity + HighChol + HighBP, data = diabetes_data) %>%
+  step_normalize(all_numeric(), -Diabetes_binary)
 
-#* Return the sum of two numbers
-#* @param a The first number to add
-#* @param b The second number to add
-#* @post /sum
-function(a, b) {
-    as.numeric(a) + as.numeric(b)
-}
+rf_spec = rand_forest(mtry = tune()) %>% 
+  set_engine("ranger") %>% 
+  set_mode("classification") %>%
+  fit(diabetes_data)
 
-# Programmatically alter your API
-#* @plumber
-function(pr) {
-    pr %>%
-        # Overwrite the default serializer to return unboxed JSON
-        pr_set_serializer(serializer_unboxed_json())
-}
+final_model = workflow() %>% 
+  add_recipe(diabetes_rec3) %>%
+  add_model(rf_spec)
+
+final_model <- workflow() %>%
+  add_recipe(diabetes_rec3) %>%
+  add_model(rand_forest(mtry = 2) %>% set_engine("ranger") %>% set_mode("classification")) %>%
+  fit(diabetes_data)
+
+
