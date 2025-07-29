c24ca9187fd5bc292282d735b8c8c412eb6417e1
ksramesh26 <keshavramesh26@gmail.com>
2025-07-29 01:42:08 -0400

    API done

--- Diabetes_Health_Indicators_ST558/plumber.R
+++ Diabetes_Health_Indicators_ST558/plumber.R
@@ -31,4 +31,42 @@ final_model = workflow() %>%
   add_model(rf_spec) %>%
   fit(diabetes_data)
 
+#* Predict diabetes probability
+#* @post /pred
+#* @param BMI:double
+#* @param AgeGroup:string
+#* @param PhysActivity:string
+#* @param HighChol:string
+#* @param HighBP:string
+predict_diabetes <- function(BMI = 27.5,
+                             AgeGroup = "60–64",
+                             PhysActivity = "Yes",
+                             HighChol = "No",
+                             HighBP = "No") {
+  
+  input_data <- tibble(
+    BMI = as.numeric(BMI),
+    AgeGroup = factor(AgeGroup, levels = c(
+      "18–24", "25–29", "30–34", "35–39", "40–44",
+      "45–49", "50–54", "55–59", "60–64", "65–69",
+      "70–74", "75–79", "80+")),
+    PhysActivity = factor(PhysActivity, levels = c("No", "Yes")),
+    HighChol = factor(HighChol, levels = c("No", "Yes")),
+    HighBP = factor(HighBP, levels = c("No", "Yes"))
+  )
+  
+  predict(final_model, new_data = input_data, type = "prob") %>%
+    bind_cols(predict(final_model, new_data = input_data)) %>%
+    rename(Probability = .pred_Diabetes, Prediction = .pred_class)
+}
+
+
+#* Model info
+#* @get /info
+get_info <- function() {
+  list(
+    name = "Keshav Ramesh",
+    github_pages = "https://ksramesh26.github.io/Final_project_ST558/EDA.html"
+  )
+}
 
